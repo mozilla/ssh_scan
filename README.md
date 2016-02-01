86bb300f8f6c83a4beafde2e80e6095313896bef
Jonathan Claudius <jclaudius@mozilla.com>
2016-02-01 16:14:32 -0500

    Add Engine and Policy components

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ A prototype/PoC for an SSH scanner
 
 Example run:
 
-    $ ruby -I ./ ssh_scan.rb
+    $ ruby -I ./ ssh_scan.rb 192.168.1.1
     {
-      "ip": "192.168.1.1",
-      "port": 22,
+      "ip": "63.245.219.51",
+      "port": "22",
       "server_banner": "SSH-2.0-OpenSSH_5.3",
       "key_algorithms": [
         "diffie-hellman-group-exchange-sha256",
@@ -83,5 +83,14 @@ Example run:
       ],
       "languages_server_to_client": [
 
-      ]
+      ],
+      "compliance": {
+        "policy": "SSH::IntermediatePolicy",
+        "compliant": false,
+        "recommendations": [
+          "Remove these Key Exchange Algos: diffie-hellman-group-exchange-sha1, diffie-hellman-group14-sha1, diffie-hellman-group1-sha1",
+          "Remove these MAC Algos: hmac-md5, hmac-sha1, umac-64@openssh.com, hmac-ripemd160, hmac-ripemd160@openssh.com, hmac-sha1-96, hmac-md5-96",
+          "Remove these Encryption Ciphers: arcfour256, arcfour128, aes128-cbc, 3des-cbc, blowfish-cbc, cast128-cbc, aes192-cbc, aes256-cbc, arcfour, rijndael-cbc@lysator.liu.se"
+        ]
+      }
     }
--- ssh_scan.rb
+++ ssh_scan.rb
@@ -1,43 +1,18 @@
 require 'protocol'
 require 'socket'
+require 'policy'
+require 'constants'
+require 'scan_engine'
 
-# Usage: ruby -I ./ ssh_scan.rb
+# Usage: ruby -I ./ ssh_scan.rb 192.168.1.1
 
-ip = "192.168.1.1"
-port = 22
-client_protocol = "SSH-2.0-client"
-key_init_raw = "d8eb97b11b6cacbc3285473f08004500019ceccf40004006663fc0a80a7c3ff" +
-               "5db33cdfd0016982e6062988da97e801810154d2b00000101080a03a6399f3d" +
-               "f735d6000001640414e33f813f8cdcc6b00a3d852ec1aea4980000001a64696" +
-               "66669652d68656c6c6d616e2d67726f7570312d736861310000000f7373682d" +
-               "6473732c7373682d727361000000576165733132382d6362632c336465732d6" +
-               "362632c626c6f77666973682d6362632c6165733139322d6362632c61657332" +
-               "35362d6362632c6165733132382d6374722c6165733139322d6374722c61657" +
-               "33235362d637472000000576165733132382d6362632c336465732d6362632c" +
-               "626c6f77666973682d6362632c6165733139322d6362632c6165733235362d6" +
-               "362632c6165733132382d6374722c6165733139322d6374722c616573323536" +
-               "2d63747200000021686d61632d6d64352c686d61632d736861312c686d61632" +
-               "d726970656d6431363000000021686d61632d6d64352c686d61632d73686131" +
-               "2c686d61632d726970656d64313630000000046e6f6e65000000046e6f6e650" +
-               "00000000000000000000000006e05b3b4"
+# Populate the info we need to perform a scan
+ip = ARGV[0].chomp
+port = ARGV[1] || 22
+policy = SSH::IntermediatePolicy.new
 
-# Do initial protocol exchange
-sock = TCPSocket.new(ip, port)
-server_protocol = sock.gets
-sock.puts(client_protocol)
+# Perform scan and get results
+scan_engine = SSH::ScanEngine.new()
+result = scan_engine.scan(ip, port, policy)
 
-# Perform Key Initialization Exchange
-sock.write(key_init_raw)
-resp = sock.read(4)
-resp += sock.read(resp.unpack("N").first)
-kex_init_response = SSH::KeyExchangeInit.read(resp)
-sock.close
-
-# Assemble and print results
-result = {
-  :ip => ip,
-  :port => port,
-  :server_banner => server_protocol.chomp
-}
-result.merge!(kex_init_response.to_hash)
 puts JSON.pretty_generate(result)
