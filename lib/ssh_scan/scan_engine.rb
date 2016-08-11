a857f296f163e3016876f6bf7dc71ea6d9d65b2a
Jinank Jain <ug201210017@iitj.ac.in>
2016-08-11 01:36:03 -0400

    Fixes #27 Add support for scanning multiple IPs

--- lib/ssh_scan/scan_engine.rb
+++ lib/ssh_scan/scan_engine.rb
@@ -6,49 +6,56 @@ module SSHScan
   class ScanEngine
 
     def scan(opts)
-      target = opts[:target]
+      targets = opts[:targets]
       port = opts[:port]
       policy = opts[:policy_file]
 
       # Connect and get results (native)
-      client = SSHScan::Client.new(target, port)
-      client.connect()
-      result = client.get_kex_result()
+      result = []
+      targets.each_with_index do |target, index|
+        client = SSHScan::Client.new(target, port)
+        client.connect()
+        result.push(client.get_kex_result())
 
-      # Connect and get results (Net-SSH)
-      net_ssh_session = Net::SSH::Transport::Session.new(target)
-      auth_session = Net::SSH::Authentication::Session.new(net_ssh_session, :auth_methods => ["none"])
-      auth_session.authenticate("none", "test", "test")
-      result['auth_methods'] = auth_session.allowed_auth_methods
-      host_key = net_ssh_session.host_keys.first
-      net_ssh_session.close
+        # Connect and get results (Net-SSH)
+        net_ssh_session = Net::SSH::Transport::Session.new(target)
+        auth_session = Net::SSH::Authentication::Session.new(net_ssh_session, :auth_methods => ["none"])
+        auth_session.authenticate("none", "test", "test")
+        result[index]['auth_methods'] = auth_session.allowed_auth_methods
+        host_key = net_ssh_session.host_keys.first
+        net_ssh_session.close
 
-      # only supporting RSA for the moment
-      unless host_key.is_a?(OpenSSL::PKey::RSA)
-        raise "Unknown host key type, need to add this host_key type"
+        # only supporting RSA for the moment
+        unless host_key.is_a?(OpenSSL::PKey::RSA)
+          raise "Unknown host key type, need to add this host_key type"
+        end
+
+        # only supporting RSA for the moment
+        unless OpenSSL::PKey::RSA
+          raise "Unknown host key type, need to add this host_key type"
+        end
+
+        data_string = OpenSSL::ASN1::Sequence([
+          OpenSSL::ASN1::Integer.new(host_key.public_key.n),
+          OpenSSL::ASN1::Integer.new(host_key.public_key.e)
+        ])
+
+        fingerprint_md5 = OpenSSL::Digest::MD5.hexdigest(data_string.to_der).scan(/../).join(':')
+        fingerprint_sha1 = OpenSSL::Digest::SHA1.hexdigest(data_string.to_der).scan(/../).join(':')
+        fingerprint_sha256 = OpenSSL::Digest::SHA256.hexdigest(data_string.to_der).scan(/../).join(':')
+
+        result[index]['fingerprints'] = {
+          "md5" => fingerprint_md5,
+          "sha1" => fingerprint_sha1,
+          "sha256" => fingerprint_sha256,
+        }
+
+        # If policy defined, then add compliance results
+        unless policy.nil?
+          policy_mgr = SSHScan::PolicyManager.new(result[index], policy)
+          result[index]['compliance'] = policy_mgr.compliance_results
+        end
       end
-
-      data_string = OpenSSL::ASN1::Sequence([
-        OpenSSL::ASN1::Integer.new(host_key.public_key.n),
-        OpenSSL::ASN1::Integer.new(host_key.public_key.e)
-      ])
-
-      fingerprint_md5 = OpenSSL::Digest::MD5.hexdigest(data_string.to_der).scan(/../).join(':')
-      fingerprint_sha1 = OpenSSL::Digest::SHA1.hexdigest(data_string.to_der).scan(/../).join(':')
-      fingerprint_sha256 = OpenSSL::Digest::SHA256.hexdigest(data_string.to_der).scan(/../).join(':')
-
-      result['fingerprints'] = {
-        "md5" => fingerprint_md5,
-        "sha1" => fingerprint_sha1,
-        "sha256" => fingerprint_sha256,
-      }
-
-      # If policy defined, then add compliance results
-      unless policy.nil?
-        policy_mgr = SSHScan::PolicyManager.new(result, policy)
-        result['compliance'] = policy_mgr.compliance_results
-      end
-
       return result
     end
   end
