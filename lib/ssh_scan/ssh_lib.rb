45209d9470412df7e4e23a50e59d40e33863193c
Jinank Jain <jinank94@gmail.com>
2016-09-02 22:43:02 -0400

    Added fingerprint for ROS

--- lib/ssh_scan/ssh_lib.rb
+++ lib/ssh_scan/ssh_lib.rb
@@ -1,4 +1,5 @@
 require 'ssh_scan/ssh_lib/openssh'
 require 'ssh_scan/ssh_lib/libssh'
 require 'ssh_scan/ssh_lib/ciscossh'
+require 'ssh_scan/ssh_lib/rosssh'
 require 'ssh_scan/ssh_lib/unknown'
---
+++ lib/ssh_scan/ssh_lib/rosssh.rb
@@ -0,0 +1,17 @@
+module SSHScan
+  module SSHLib
+    class ROSSSH
+      def common
+        "rosssh"
+      end
+
+      def cpe
+        "a:rosssh:rosssh"
+      end
+
+      def version
+        nil
+      end
+    end
+  end
+end
