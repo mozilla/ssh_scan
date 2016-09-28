5c07d60c3b33db510dd4665cc0d84e4052a5508f
Jonathan Claudius <jclaudius@mozilla.com>
2016-09-27 22:24:59 -0400

    Add openssh unit-tests

---
+++ spec/ssh_scan/banner/ssh_lib/openssh_spec.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+require_relative '../helper'
+
+describe SSHScan::Banner do
+  context "when openssh fingerprinting" do
+    expectations = {
+      "SSH-2.0-OpenSSH_7.3" => {
+        :ssh_lib_class => SSHScan::SSHLib::OpenSSH,
+        :ssh_lib_version => "7.3",
+      },
+      "SSH-2.0-OpenSSH_6.8p1-hpn14v6" => {
+        :ssh_lib_class => SSHScan::SSHLib::OpenSSH,
+        :ssh_lib_version => "6.8p1",
+      },
+      "SSH-2.0-OpenSSH_6.6.1" => {
+        :ssh_lib_class => SSHScan::SSHLib::OpenSSH,
+        :ssh_lib_version => "6.6.1",
+      },
+      "SSH-2.0-OpenSSH_6.2 FIPS" => {
+        :ssh_lib_class => SSHScan::SSHLib::OpenSSH,
+        :ssh_lib_version => "6.2",
+      },
+      "SSH-2.0-OpenSSH_6.2 FIPS" => {
+        :ssh_lib_class => SSHScan::SSHLib::OpenSSH,
+        :ssh_lib_version => "6.2",
+      },
+      "SSH-2.0-OpenSSH_12.1" => {
+        :ssh_lib_class => SSHScan::SSHLib::OpenSSH,
+        :ssh_lib_version => "12.1",
+      },
+      "SSH-1.99-OpenSSH_3.7.1p2" => {
+        :ssh_lib_class => SSHScan::SSHLib::OpenSSH,
+        :ssh_lib_version => "3.7.1p2",
+      },
+    }
+    checkFingerprints(expectations)
+  end
+end
