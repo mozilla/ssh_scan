b8c1e33a075ade7d4977839a976c506e4cfdd737
Vishal Pandey <vishpandey2014@gmail.com>
2016-10-04 02:59:39 +0530

    specs for string_ext

--- spec/ssh_scan/string_ext_spec.rb
+++ spec/ssh_scan/string_ext_spec.rb
@@ -13,47 +13,47 @@ describe String do
     end
   end
 
-	context "when hexing a string" do
-	testing_sample = "Hello World!"
-		it "should load all the attributes properly" do
-		  expected_hexedstr = "48656c6c6f20576f726c6421"
-		  test_result = testing_sample.hexify
-		  expect(test_result).to eql(expected_hexedstr)
-		end
-	end
+    context "when hexing a string" do
+    testing_sample = "Hello World!"
+        it "should load all the attributes properly" do
+          expected_hexedstr = "48656c6c6f20576f726c6421"
+          test_result = testing_sample.hexify
+          expect(test_result).to eql(expected_hexedstr)
+        end
+    end
 
-	context "when verifying an IP address" do
-	testing_target = "127.0.0.1"
-		it "should load all the attributes properly" do
-		  expected_result = true
-		  test_result = testing_target.ip_addr?
-		  expect(test_result).to eql(true)
-		end
-	end
+    context "when verifying an IP address" do
+    testing_target = "127.0.0.1"
+        it "should load all the attributes properly" do
+          expected_result = true
+          test_result = testing_target.ip_addr?
+          expect(test_result).to eql(true)
+        end
+    end
 
-	context "when resolving a DNS name as IPv4" do
-	testing_dns = "192.168.1.1"
-		it "should load all the attributes properly" do
-		  test_result = testing_dns.resolve_fqdn_as_ipv4.to_s
-		  expect(test_result).to eql("192.168.1.1")
-		end
-	end
+    context "when resolving a DNS name as IPv4" do
+    testing_dns = "192.168.1.1"
+        it "should load all the attributes properly" do
+          test_result = testing_dns.resolve_fqdn_as_ipv4.to_s
+          expect(test_result).to eql("192.168.1.1")
+        end
+    end
 
-	context "when resolving a DNS name into IP adress" do
-	testing_host = "localhost"
-		it "should load all the attributes properly" do
-		  expected_result = "::1"
-		  test_result = testing_host.resolve_fqdn
-		  expect(test_result).to eql(expected_result)
-		end
-	end
+    context "when resolving a DNS name into IP adress" do
+    testing_host = "localhost"
+        it "should load all the attributes properly" do
+          expected_result = "::1"
+          test_result = testing_host.resolve_fqdn
+          expect(test_result).to eql(expected_result)
+        end
+    end
 
-	context "when verifying a DNS name and IP address" do
-	testing_dns = "google.com"
-		it "should load all the attributes properly" do
-		  test_result = testing_dns.fqdn?
-		  expect(test_result).to eql(true)
-		end
-	end
+    context "when verifying a DNS name and IP address" do
+    testing_dns = "google.com"
+        it "should load all the attributes properly" do
+          test_result = testing_dns.fqdn?
+          expect(test_result).to eql(true)
+        end
+    end
 
 end
