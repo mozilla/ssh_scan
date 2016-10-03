15a3cbd2a0773e9a08663e4dcd4e7211c3e34191
Vishal Pandey <vishpandey2014@gmail.com>
2016-10-04 02:32:05 +0530

    specs for string_ext

--- spec/ssh_scan/string_ext_spec.rb
+++ spec/ssh_scan/string_ext_spec.rb
@@ -1,7 +1,5 @@
 require 'rspec'
 require 'pathname'
-require 'ipaddr'
-require 'resolv'
 require 'string_ext'
 
 describe String do
@@ -33,26 +31,25 @@ describe String do
 	end
 
 	context "when resolving a DNS name as IPv6" do
-	testing_dns = "google.com"
+	testing_dns = ""
 		it "should load all the attributes properly" do
-		  expected_result = Resolv::IPv6.create("2404:6800:4007:802::200E")
-		  test_result = testing_dns.resolve_fqdn_as_ipv6
-		  expect(test_result).to eql(expected_result)
+		  test_result = testing_dns.resolve_fqdn_as_ipv6.to_s
+		  expect(test_result).to eql("2607:F8B0:4006:80F::200E")
 		end
 	end
+
 	context "when resolving a DNS name as IPv4" do
-	testing_dns = "google.com"
+	testing_dns = "www.google.com"
 		it "should load all the attributes properly" do
-		  expected_result = Resolv::IPv4.create("172.217.26.206")
 		  test_result = testing_dns.resolve_fqdn_as_ipv4
-		  expect(test_result).to eql(expected_result)
+		  expect(test_result).to eql("172.217.3.14")
 		end
 	end
 
 	context "when resolving a DNS name into IP adress" do
 	testing_host = "localhost"
 		it "should load all the attributes properly" do
-		  expected_result = "127.0.0.1"
+		  expected_result = "::1"
 		  test_result = testing_host.resolve_fqdn
 		  expect(test_result).to eql(expected_result)
 		end
