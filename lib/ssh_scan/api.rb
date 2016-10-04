51b9f6f8417f17ea9def8d1c8205b3498a646c21
Jonathan Claudius <jclaudius@mozilla.com>
2016-10-04 00:12:50 -0400

    Customize headers

--- lib/ssh_scan/api.rb
+++ lib/ssh_scan/api.rb
@@ -15,11 +15,8 @@ module SSHScan
       config.cookies = {
         secure: true, # mark all cookies as "Secure"
         httponly: true, # mark all cookies as "HttpOnly"
-        samesite: {
-          lax: true # mark all cookies as SameSite=lax
-        }
       }
-      #config.hsts = "max-age=#{20.years.to_i}; includeSubdomains; preload"
+      config.hsts = "max-age=31536000; includeSubdomains; preload"
       config.x_frame_options = "DENY"
       config.x_content_type_options = "nosniff"
       config.x_xss_protection = "1; mode=block"
@@ -27,43 +24,23 @@ module SSHScan
       config.x_permitted_cross_domain_policies = "none"
       config.referrer_policy = "origin-when-cross-origin"
       config.csp = {
-        # "meta" values. these will shaped the header, but the values are not included in the header.
-        report_only: true,      # default: false [DEPRECATED from 3.5.0: instead, configure csp_report_only]
-        preserve_schemes: true, # default: false. Schemes are removed from host sources to save bytes and discourage mixed content.
-
-        # directive values: these values will directly translate into source directives
         default_src: %w(https: 'self'),
         base_uri: %w('self'),
         block_all_mixed_content: true, # see http://www.w3.org/TR/mixed-content/
-        child_src: %w('self'), # if child-src isn't supported, the value for frame-src will be set.
-        connect_src: %w(wss:),
-        font_src: %w('self' data:),
-        form_action: %w('self' github.com),
+        child_src: %w('none'), # if child-src isn't supported, the value for frame-src will be set.
+        connect_src: %w('none'),
+        font_src: %w('none'),
+        form_action: %w('none'),
         frame_ancestors: %w('none'),
-        img_src: %w(mycdn.com data:),
-        media_src: %w(utoob.com),
-        object_src: %w('self'),
-        plugin_types: %w(application/x-shockwave-flash),
-        script_src: %w('self'),
-        style_src: %w('unsafe-inline'),
+        img_src: %w('none'),
+        media_src: %w('none'),
+        object_src: %w('none'),
+        plugin_types: %w('none'),
+        script_src: %w('none'),
+        style_src: %w('none'),
         upgrade_insecure_requests: true, # see https://www.w3.org/TR/upgrade-insecure-requests/
-        report_uri: %w(https://report-uri.io/example-csp)
+        #report_uri: %w(https://report-uri.io/example-csp)
       }
-      # This is available only from 3.5.0; use the `report_only: true` setting for 3.4.1 and below.
-      # config.csp_report_only = config.csp.merge({
-      #   img_src: %w(somewhereelse.com),
-      #   report_uri: %w(https://report-uri.io/example-csp-report-only)
-      # })
-      # config.hpkp = {
-      #   report_only: false,
-      #   max_age: 60.days.to_i,
-      #   include_subdomains: true,
-      #   report_uri: "https://report-uri.io/example-hpkp",
-      #   pins: [
-      #     {sha256: "abc"},
-      #     {sha256: "123"}
-      #   ]
-      # }
     end
 
     class NullLogger < Logger
@@ -75,6 +52,10 @@ module SSHScan
     end
 
     register Sinatra::Namespace
+
+    before do
+      headers "Server" => "ssh_scan_api"
+    end
 
     # Custom 404 handling
     not_found do
