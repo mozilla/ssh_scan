5ad23724530b3a80747beb79b8046f43e80f7710
Rishabh Saxena <orishabh1@gmail.com>
2017-01-07 11:03:31 +0530

    api_spec: update tests travis: add db configs

--- lib/ssh_scan/api.rb
+++ lib/ssh_scan/api.rb
@@ -18,6 +18,7 @@ module SSHScan
       configure do
         set :job_queue, JobQueue.new()
         set :authentication, false
+        set :db, SSHScan::DatabaseConfig.from_config_file
       end
     end
 
