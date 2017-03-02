883833ad434764e09f3ac430382e8916fa849e2b
Jonathan Claudius <jclaudius@mozilla.com>
2017-03-02 00:00:24 -0500

    More prep for api project split

--- spec/ssh_scan/version_spec.rb
+++ spec/ssh_scan/version_spec.rb
@@ -17,17 +17,4 @@ describe SSHScan::VERSION do
       expect(octet.to_i).to be <= 20
     end
   end
-
-  it "SSHScan::API_VERSION should be a string" do
-    expect(SSHScan::API_VERSION).to be_kind_of(::String)
-  end
-
-  it "SSHScan::API_VERSION should have 3 levels" do
-    expect(SSHScan::API_VERSION.split('.').size).to eql(1)
-  end
-
-  it "SSHScan::API_VERSION should have a number between 1-20 for each octet" do
-    expect(SSHScan::API_VERSION).to eql("1")
-  end
-
 end
