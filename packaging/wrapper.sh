801eb82a771f8a3b85515711dc61729e495687c9
Rishabh Saxena <orishabh1@gmail.com>
2017-05-24 12:45:52 +0530

    rake-task: package fix

---
+++ packaging/wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Figure out where this script is located.
+SELFDIR="`dirname \"$0\"`"
+SELFDIR="`cd \"$SELFDIR\" && pwd`"
+
+# Tell Bundler where the Gemfile and gems are.
+export BUNDLE_GEMFILE="$SELFDIR/lib/vendor/Gemfile"
+unset BUNDLE_IGNORE_CONFIG
+
+# Run the actual app using the bundled Ruby interpreter, with Bundler activated.
+exec "$SELFDIR/lib/ruby/bin/ruby" -rbundler/setup "$SELFDIR/lib/app/ssh_scan"
